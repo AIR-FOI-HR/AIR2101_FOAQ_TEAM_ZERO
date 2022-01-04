b634ee7b11d3f84d33860f41b6d52f4cf7a49baf
msakac <msakac@foi.hr>
2022-01-04 21:38:55 +0100

    Added three new museum to see if SingleChildScrollView works

--- lib/providers/museums.dart
+++ lib/providers/museums.dart
@@ -11,7 +11,7 @@ class Museums with ChangeNotifier {
     Museum(
         id: '1',
         name: 'Gladiator Museum',
-        address: 'Piazza Navona 90, Roma, Italie',
+        address: 'Piazza Navona 90, Roma, Italy',
         description:
             'The exposition represents an itinerary through history going from the VII century B.C. to the IV century A.D. brought to life through the accurate reconstruction of helmets and armour of the Roman legions and gladiators, showing over a millenium of history through the most representative weaponry of Roman army and its enemies. Set on two floors, in the basement there is the chance to see and touch the autentique columns of the ancient Stadium of Domitian and to take photos into the gladiators’ prison.',
         tourDuration: 45,
@@ -19,7 +19,7 @@ class Museums with ChangeNotifier {
     Museum(
         id: '2',
         name: 'Doria Pamhili Gallery',
-        address: 'Via del Corso 305, Roma, Italie',
+        address: 'Via del Corso 305, Roma, Italy',
         description:
             'The exposition represents an itinerary through history going from the VII century B.C. to the IV century A.D. brought to life through the accurate reconstruction of helmets and armour of the Roman legions and gladiators, showing over a millenium of history through the most representative weaponry of Roman army and its enemies. Set on two floors, in the basement there is the chance to see and touch the autentique columns of the ancient Stadium of Domitian and to take photos into the gladiators’ prison.',
         tourDuration: 50,
@@ -27,16 +27,39 @@ class Museums with ChangeNotifier {
     Museum(
         id: '3',
         name: 'Capitoline Museum',
-        address: 'Piazza del Campidoglio 1, Roma, Italie',
+        address: 'Piazza del Campidoglio 1, Roma, Italy',
         description:
             'The exposition represents an itinerary through history going from the VII century B.C. to the IV century A.D. brought to life through the accurate reconstruction of helmets and armour of the Roman legions and gladiators, showing over a millenium of history through the most representative weaponry of Roman army and its enemies. Set on two floors, in the basement there is the chance to see and touch the autentique columns of the ancient Stadium of Domitian and to take photos into the gladiators’ prison.',
         tourDuration: 30,
         imageUrl: 'assets/images/Capitolini.png'),
+    Museum(
+        id: '4',
+        name: 'Uffizi',
+        address: 'Piazzale degli Uffizi 6, Firenze, Italy',
+        description:
+            'The exposition represents an itinerary through history going from the VII century B.C. to the IV century A.D. brought to life through the accurate reconstruction of helmets and armour of the Roman legions and gladiators, showing over a millenium of history through the most representative weaponry of Roman army and its enemies. Set on two floors, in the basement there is the chance to see and touch the autentique columns of the ancient Stadium of Domitian and to take photos into the gladiators’ prison.',
+        tourDuration: 30,
+        imageUrl: 'assets/images/Uffizi.png'),
+    Museum(
+        id: '5',
+        name: 'Brera',
+        address: 'Via Brera 28, Milano, Italy',
+        description:
+            'The exposition represents an itinerary through history going from the VII century B.C. to the IV century A.D. brought to life through the accurate reconstruction of helmets and armour of the Roman legions and gladiators, showing over a millenium of history through the most representative weaponry of Roman army and its enemies. Set on two floors, in the basement there is the chance to see and touch the autentique columns of the ancient Stadium of Domitian and to take photos into the gladiators’ prison.',
+        tourDuration: 30,
+        imageUrl: 'assets/images/Brera.png'),
+    Museum(
+        id: '6',
+        name: 'Museum Egizio',
+        address: 'Via Accademia delle Scienze 6, Torino, Italy',
+        description:
+            'The exposition represents an itinerary through history going from the VII century B.C. to the IV century A.D. brought to life through the accurate reconstruction of helmets and armour of the Roman legions and gladiators, showing over a millenium of history through the most representative weaponry of Roman army and its enemies. Set on two floors, in the basement there is the chance to see and touch the autentique columns of the ancient Stadium of Domitian and to take photos into the gladiators’ prison.',
+        tourDuration: 30,
+        imageUrl: 'assets/images/Egizio.png'),
   ];
-  
-  //getter that returns all museum in the list
-  List<Museum> get getMuseums{
-    return [... _museums];
-  }
 
+  //getter that returns all museum in the list
+  List<Museum> get getMuseums {
+    return [..._museums];
+  }
 }
