42ca3791513882c6153f8e71a7cf26ac7f3fde78
msakac <msakac@foi.hr>
2022-01-04 18:09:30 +0100

    Created museums_grid.dart. This file contains widget 'MuseumsGrid'. In this file we have a GridView.builder with design and MuseumGridItem(). For every museum (from Provider.of<Museums>) we have a child MuseumGridItem().

---
+++ lib/widgets/homepage/museums_grid.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../providers/museums.dart';
+import './museum_grid_item.dart';
+
+class MuseumsGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final museumsData =
+        Provider.of<Museums>(context).getMuseums; //access to museums data
+    return GridView.builder(
+      padding: EdgeInsets.all(10),
+      itemCount: museumsData.length,
+      itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
+        value: museumsData[i],
+        child: MuseumGridItem(),
+      ),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 1,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 15,
+        mainAxisSpacing: 15,
+      ),
+    );
+  }
+}
