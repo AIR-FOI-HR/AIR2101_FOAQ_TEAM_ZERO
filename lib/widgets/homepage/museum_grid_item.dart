278d1cf519714d9960b6728503deecfe60f2fa65
msakac <msakac@foi.hr>
2022-01-04 18:07:31 +0100

    Created file museum_grid_item.dart. This file contains widget 'MuseumGridItem'. This item is used in 'MuseumGrid' to display museum name, address and image.

---
+++ lib/widgets/homepage/museum_grid_item.dart
@@ -0,0 +1,64 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../models/museum.dart';
+
+class MuseumGridItem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final museum = Provider.of<Museum>(context, listen: false);
+    final city = museum.address
+        .substring(museum.address.indexOf(',') + 1, museum.address.length);
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(5),
+      child: GridTile(
+        child: GestureDetector(
+            onTap: () {
+              //TO DO WHEN TAPPED ON MUSEUM
+            },
+            child: Image.asset(museum.imageUrl, fit: BoxFit.cover)),
+        footer: GridTileBar(
+          backgroundColor: Colors.black54,
+          leading: RichText(
+            text: TextSpan(
+              children: [
+                WidgetSpan(
+                  child: Icon(
+                    Icons.pin_drop_outlined,
+                    size: 20,
+                    color: Theme.of(context).accentColor,
+                  ),
+                ),
+                TextSpan(
+                    text: city,
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Theme.of(context).accentColor,
+                    )),
+              ],
+            ),
+          ),
+        ),
+        header: GridTileBar(
+          trailing: Container(
+            padding: EdgeInsets.all(5),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5),
+              color: Colors.black.withOpacity(0.7),
+            ),
+            child: Text(
+              '${museum.name}',
+              style: TextStyle(
+                fontSize: 18,
+                color: Theme.of(context).accentColor,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
