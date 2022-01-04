0c1e9e2c5fdf527eea3807e1c12478e3d37ba9f1
msakac <msakac@foi.hr>
2022-01-04 21:41:02 +0100

    Changed body of MuseumsOverviewScreen. Now in body we have SingleChildScrollView. Inside it we have a Column that has a Row and MuseumsGrid. In the Row we have two new widgets DropDownCategory() and Search() widget.

--- lib/screens/museums_overview_screen.dart
+++ lib/screens/museums_overview_screen.dart
@@ -1,14 +1,30 @@
 import 'package:flutter/material.dart';
 import '../widgets/homepage/museums_grid.dart';
+import '../widgets/homepage/search_bar.dart';
+import '../widgets/homepage/dropdown_category.dart';
 
 class MuseumsOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Museums'),
+        title: const Text('Museum app'),
+        backgroundColor: Theme.of(context).primaryColor,
       ),
-      body: MuseumsGrid(),
+      body: SingleChildScrollView( //remove this if search is fixed
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                DropDownCategory(),
+                SearchBar(),
+              ],
+            ),
+             MuseumsGrid(), //wrap with flexible if search is fixed
+          ],
+        ),
+      ),
     );
   }
 }
