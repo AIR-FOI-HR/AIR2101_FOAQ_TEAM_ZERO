24a2ae0f4a86762cce942683628ea41334882254
msakac <msakac@foi.hr>
2022-01-05 12:21:32 +0100

    Changed app bar font size

--- lib/widgets/app_bar.dart
+++ lib/widgets/app_bar.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 
 AppBar appBar(String title, BuildContext ctx) {
   return AppBar(
-    title: Text(title),
+    title: Text(
+      title,
+      style: TextStyle(fontSize: 20),
+    ),
     backgroundColor: Theme.of(ctx).primaryColor,
   );
 }
