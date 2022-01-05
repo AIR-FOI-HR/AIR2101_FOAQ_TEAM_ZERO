409f39c5f7faa92b094a64df9fe509b4d069ba4d
Tom1sl4v97 <tomo.tomiek97@gmail.com>
2022-01-05 15:17:12 +0100

    Creating the screen for the display of categories.

---
+++ lib/screens/categories/category_artwork_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import '../../widgets/app_bar_property.dart';
+
+class CategoryArtworkScreen extends StatelessWidget {
+  static const routeName = '/categoryArtwork';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Museum app'),
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
+      body: Text('halp'),
+    );
+  }
+}
