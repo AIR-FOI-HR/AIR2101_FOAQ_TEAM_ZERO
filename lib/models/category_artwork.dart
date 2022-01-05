9754d110c810cbf85d0903bff4ba9d248879c818
Tom1sl4v97 <tomo.tomiek97@gmail.com>
2022-01-05 15:14:31 +0100

    Adding the index.

---
+++ lib/models/category_artwork.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class CategoryArtwork {
+  final String id;
+  final String name;
+
+  CategoryArtwork({
+    @required this.id,
+    @required this.name,
+  });
+}
