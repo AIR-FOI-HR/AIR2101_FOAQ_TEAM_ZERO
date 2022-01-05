98d92a3988d8d3e83646fc81a1cc987720d9b88b
msakac <msakac@foi.hr>
2022-01-05 08:55:31 +0100

    Added static const routeName

--- lib/screens/museum_detail_screen.dart
+++ lib/screens/museum_detail_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class MuseumDetailScreen extends StatelessWidget {
+  static const routeName ='/museum-detail'; //namedroute for pushing named from MuseumOverviewScree
 
   @override
   Widget build(BuildContext context) {
