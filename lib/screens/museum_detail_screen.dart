7b1b43d4c803354d23d85372cb126466b8105e43
msakac <msakac@foi.hr>
2022-01-05 08:49:11 +0100

    Created 'museum_detail_screen.dart' file

---
+++ lib/screens/museum_detail_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class MuseumDetailScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
