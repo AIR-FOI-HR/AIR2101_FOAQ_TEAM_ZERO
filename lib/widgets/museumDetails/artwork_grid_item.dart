0819cdceed36b5a7598f630dc2dc378fb43fadc0
msakac <msakac@foi.hr>
2022-01-06 10:18:32 +0100

    Created widget that displays single artwork gridtile. This widget is used in artworks grid

---
+++ lib/widgets/museumDetails/artwork_grid_item.dart
@@ -0,0 +1,60 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../models/artwork.dart';
+
+class ArtworkGridItem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final artwork = Provider.of<Artwork>(context, listen: false);
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(5),
+      child: GridTile(
+        child: Image.network(artwork.imageUrl, fit: BoxFit.cover),
+        footer: GridTileBar(
+          backgroundColor: Colors.black54,
+          leading: RichText(
+            text: TextSpan(
+              children: [
+                WidgetSpan(
+                  child: Icon(
+                    Icons.person,
+                    size: 20,
+                    color: Theme.of(context).accentColor,
+                  ),
+                ),
+                TextSpan(
+                    text: artwork.author,
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Theme.of(context).accentColor,
+                      fontWeight: FontWeight.w300
+                    )),
+              ],
+            ),
+          ),
+        ),
+        header: GridTileBar(
+          trailing: Container(
+            padding: EdgeInsets.all(5),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5),
+              color: Colors.black.withOpacity(0.7),
+            ),
+            child: Text(
+              '${artwork.name}',
+              style: TextStyle(
+                fontSize: 18,
+                color: Theme.of(context).highlightColor,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
