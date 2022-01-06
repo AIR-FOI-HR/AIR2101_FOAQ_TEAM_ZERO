18a9548e2d66af8247ac65a14a5aec034f7b715f
msakac <msakac@foi.hr>
2022-01-06 10:31:49 +0100

    Added if statement that checks if museum has artworks. If not it renders text widget (TO-DO style if museum has no artworks)

--- lib/widgets/museumDetails/artworks_grid.dart
+++ lib/widgets/museumDetails/artworks_grid.dart
@@ -5,7 +5,6 @@ import 'package:provider/provider.dart';
 import '../../providers/artworks.dart';
 import './artwork_grid_item.dart';
 
-
 class ArtworksGrid extends StatelessWidget {
   //this widget gets museum id so we can find that artworks that belong to this museum
   final museumId;
@@ -13,23 +12,27 @@ class ArtworksGrid extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-      print(museumId);
-    final museumArtworks =
-        Provider.of<Artworks>(context).getByMuseumId(museumId); //access to museum artworks
-    return GridView.builder(
-      shrinkWrap: true, //remove this if search is fixed
-      physics: const NeverScrollableScrollPhysics(),//remove this if search is fixed
-      padding: const EdgeInsets.all(10),
-      itemCount: museumArtworks.length,
-      itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
-        value: museumArtworks[i],
-        child: ArtworkGridItem(),
-      ),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 1,
-        childAspectRatio: 3 / 2,
-        mainAxisSpacing: 10,
-      ),
-    );
+    print(museumId);
+    final museumArtworks = Provider.of<Artworks>(context)
+        .getByMuseumId(museumId); //access to museum artworks
+    return !museumArtworks.isEmpty
+        ? GridView.builder(
+            shrinkWrap: true, //remove this if search is fixed
+            physics:
+                const NeverScrollableScrollPhysics(), //remove this if search is fixed
+            padding: const EdgeInsets.all(10),
+            itemCount: museumArtworks.length,
+            itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
+              value: museumArtworks[i],
+              child: ArtworkGridItem(),
+            ),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 1,
+              childAspectRatio: 3 / 2,
+              mainAxisSpacing: 10,
+              crossAxisSpacing: 10,
+            ),
+          )
+        : Text('${museumId} has no artworks!'); //TO DO 
   }
 }
