08872d5cc9eb4303bc45b68dcb0f2bb851997224
msakac <msakac@foi.hr>
2022-01-06 10:19:09 +0100

    Added ArtworksGrid() widget at the end of the screen. It is scrollable

--- lib/screens/museum_detail_screen.dart
+++ lib/screens/museum_detail_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../widgets/app_bar.dart';
+import '../widgets/museumDetails/artworks_grid.dart';
 
 import '../providers/museums.dart';
 
@@ -164,6 +165,7 @@ class MuseumDetailScreen extends StatelessWidget {
                   textAlign: TextAlign.center,
                   style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
             ),
+            ArtworksGrid(museumId),
           ],
         ),
       ),
