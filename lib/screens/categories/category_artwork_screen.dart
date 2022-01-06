5311271b6a63f585b24519fbd28bd4ce021efa8b
Tom1sl4v97 <tomo.tomiek97@gmail.com>
2022-01-06 17:51:28 +0100

    test comit

--- lib/screens/categories/category_artwork_screen.dart
+++ lib/screens/categories/category_artwork_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../../widgets/app_bar_property.dart';
+//import '../../widgets/app_bar_property.dart';
 
 class CategoryArtworkScreen extends StatelessWidget {
   static const routeName = '/categoryArtwork';
