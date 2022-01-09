a41e1f955782f00a19dbf7ede8c840c31d03546c
msakac <msakac@foi.hr>
2022-01-09 14:19:25 +0100

    Added icon button. Added consumer widget that rebuilds only iconbutton when pressed.

--- lib/widgets/museumDetails/artwork_grid_item.dart
+++ lib/widgets/museumDetails/artwork_grid_item.dart
@@ -14,22 +14,30 @@ class ArtworkGridItem extends StatelessWidget {
       borderRadius: BorderRadius.circular(5),
       child: GridTile(
         child: Image.network(artwork.imageUrl, fit: BoxFit.cover),
-        header: Container( //maybe have another screen where we display artwork info
-          padding: EdgeInsets.all(10),
+        header: Container(
+          //maybe have another screen where we display artwork info
+          padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
           child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Flexible(
-                child: Text(
-                  artwork.author,
-                  softWrap: true,
-                  style: TextStyle(
-                    color: Theme.of(context).highlightColor,
-                    backgroundColor: Colors.black87,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 13,
-                  ),
+              Text(
+                artwork.author,
+                softWrap: true,
+                style: TextStyle(
+                  color: Theme.of(context).highlightColor,
+                  backgroundColor: Colors.black87,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 13,
                 ),
               ),
+              Consumer<Artwork>(
+                builder: (ctx, artwork, child) =>  IconButton(
+                  onPressed: () {
+                    artwork.toggleFavorite();
+                  },
+                  icon: Icon(artwork.isFavorite ? Icons.favorite :Icons.favorite_border ,color: Theme.of(context).accentColor,size: 30,),
+                ),
+              )
             ],
           ),
         ),
