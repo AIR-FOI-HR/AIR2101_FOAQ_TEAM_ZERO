0c5f277628b824489c21eb3fc51a7935711a78ea
Tom1sl4v97 <tomo.tomiek97@gmail.com>
2022-01-09 22:04:12 +0100

    Make changes to the dynamic height of the screen, change the look of the screen and add options for editing and displaying category screen

--- lib/screens/categories/category_artwork_screen.dart
+++ lib/screens/categories/category_artwork_screen.dart
@@ -2,28 +2,39 @@ import 'package:flutter/material.dart';
 import '../../widgets/app_bar.dart';
 import '../../widgets/main_menu_drawer.dart';
 import '../../widgets/categories/category_artwork_grid.dart';
+import '../../widgets/categories/save_button.dart';
+import '../../widgets/categories/add_new_category_button.dart';
 
 class CategoryArtworkScreen extends StatelessWidget {
   static const routeName = '/categoryArtwork';
+
   @override
   Widget build(BuildContext context) {
+    final int privileges = 1;
+    final appBarProperty =
+        appBar('Categories', context, Theme.of(context).primaryColor);
+    final mediaQuery = MediaQuery.of(context);
     return Scaffold(
-      appBar: appBar('Categories', context, Theme.of(context).primaryColor),
+      appBar: appBarProperty,
       drawer: MainMenuDrawer(),
-      body: CategoryArtworkGrid(),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Theme.of(context).highlightColor,
-        onPressed: () {
-          Navigator.of(context).pushReplacementNamed('/');
-        },
-        elevation: 5,
-        child: Icon(
-          Icons.save,
-          size: 40,
-          color: Theme.of(context).primaryColor,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: (mediaQuery.size.height -
+                      appBarProperty.preferredSize.height -
+                      mediaQuery.padding.top) *
+                  0.87,
+              child: CategoryArtworkGrid(),
+            ),
+            Divider(
+              thickness: 2,
+              color: Theme.of(context).primaryColor,
+            ),
+            privileges == 1 ? AddNewCategoryButton() : SaveButton(),
+          ],
         ),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
