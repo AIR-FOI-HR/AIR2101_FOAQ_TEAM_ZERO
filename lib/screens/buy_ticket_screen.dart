683d0d9028a77a326a602410d40b3f7cfac5c0ee
msakac <msakac@foi.hr>
2022-01-09 10:56:49 +0100

    Added appBar from wigets

--- lib/screens/buy_ticket_screen.dart
+++ lib/screens/buy_ticket_screen.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/app_bar.dart';
+
 class BuyTicketScreen extends StatelessWidget {
   static const routeName="/buy-tickets"; //namedroute for buy ticket screen
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Buy ticket'),),
+      appBar: appBar('Buy tickets', context, Theme.of(context).primaryColor)
     );
   }
 }
