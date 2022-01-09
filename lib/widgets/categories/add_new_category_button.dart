6ab9737e7ce1b1e7c4c0ad94bbbdc22efe2984eb
Tom1sl4v97 <tomo.tomiek97@gmail.com>
2022-01-09 22:07:40 +0100

    Creting the button widget for adding new category

---
+++ lib/widgets/categories/add_new_category_button.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import '../../screens/categories/category_artwork_editing_screen.dart';
+
+class AddNewCategoryButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      child: Text(
+        'Add a new category',
+        style: Theme.of(context).textTheme.headline3,
+      ),
+      style: TextButton.styleFrom(
+        backgroundColor: Theme.of(context).highlightColor,
+        shape: const BeveledRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(5))),
+      ),
+      onPressed: () {
+        Navigator.of(context)
+            .pushNamed(CategoryArtworkEditingScreen.routeName, arguments: null);
+      },
+    );
+  }
+}
