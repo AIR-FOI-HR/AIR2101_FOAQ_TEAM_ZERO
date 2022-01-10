852dcb8c1b36d9c2db8f1b5144d7706b9a7396ee
Tom1sl4v97 <tomo.tomiek97@gmail.com>
2022-01-10 16:58:24 +0100

    Adding the action button for user login

--- lib/widgets/app_bar.dart
+++ lib/widgets/app_bar.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
+import '../screens/login/login_screen.dart';
 
 AppBar appBar(String title, BuildContext ctx, Color color) {
   return AppBar(
     title: Text(title),
     backgroundColor: color,
+    actions: title != 'Login'
+        ? <Widget>[
+            IconButton(
+              color: Colors.white,
+              icon: const Icon(Icons.login),
+              iconSize: 40,
+              tooltip: 'Go to login screen',
+              onPressed: () {
+                Navigator.of(ctx).pushReplacementNamed(LoginScreen.routeName);
+              },
+            ),
+          ]
+        : null,
   );
 }
