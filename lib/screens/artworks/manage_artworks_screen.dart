6025ddcf5f06a74e951842d0102c9d8547280e12
msakac <msakac@foi.hr>
2022-01-10 23:08:50 +0100

    Added drawer

--- lib/screens/artworks/manage_artworks_screen.dart
+++ lib/screens/artworks/manage_artworks_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../widgets/app_bar.dart';
+import '../../widgets/main_menu_drawer.dart';
 
 class ManageArtworksScreen extends StatelessWidget {
   static const routeName ='/artworks';
@@ -9,6 +10,7 @@ class ManageArtworksScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar('Artworks', context, Theme.of(context).primaryColor),
+      drawer: MainMenuDrawer()
     );
   }
 }
