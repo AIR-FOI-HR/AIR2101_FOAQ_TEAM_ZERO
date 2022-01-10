ca3e90d26845721f46556c751c435451234e53f3
Tom1sl4v97 <tomo.tomiek97@gmail.com>
2022-01-10 20:55:31 +0100

    Creating the login screen

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,15 +1,44 @@
 import 'package:flutter/material.dart';
 import '../../widgets/app_bar.dart';
 import '../../widgets/main_menu_drawer.dart';
+import '../../widgets/login/display_logo.dart';
+import '../../widgets/login/login_input_design.dart';
 
 class LoginScreen extends StatelessWidget {
   static const routeName = '/userLogin';
 
   @override
   Widget build(BuildContext context) {
+    final appBarProperty =
+        appBar('Login', context, Theme.of(context).primaryColor);
+    final mediaQuery = MediaQuery.of(context);
     return Scaffold(
-      appBar: appBar('Login', context, Theme.of(context).primaryColor),
-      drawer: MainMenuDrawer(),
-    );
+        appBar: appBarProperty,
+        drawer: MainMenuDrawer(),
+        body: SingleChildScrollView(
+          child: Container(
+            margin: EdgeInsets.only(left: 30, right: 30, bottom: 50),
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                    height: (mediaQuery.size.height -
+                            appBarProperty.preferredSize.height -
+                            mediaQuery.padding.top) *
+                        0.2,
+                    child: DisplayLogo()),
+                Container(
+                  height: (mediaQuery.size.height -
+                          appBarProperty.preferredSize.height -
+                          mediaQuery.padding.top) *
+                      0.7,
+                  child: LoginInputDesign(),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
