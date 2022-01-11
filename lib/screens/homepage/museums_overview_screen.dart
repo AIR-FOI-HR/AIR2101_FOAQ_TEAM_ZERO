e5fce8ca5c5193c7127c055f5eed6533325eaa1d
msakac <msakac@foi.hr>
2022-01-11 11:23:42 +0100

    Converted to StatefulWidget. Added two list one for search and for search result. Added didChangeDependecies that gets museums. Added searchMuseum method that take query String as argument. This method is passed to searchBar widget. This method return search query result and stores it in list of museums.

--- lib/screens/homepage/museums_overview_screen.dart
+++ lib/screens/homepage/museums_overview_screen.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import '../../widgets/homepage/museums_grid.dart';
 import '../../widgets/homepage/search_bar.dart';
 import '../../widgets/homepage/dropdown_category.dart';
 import '../../widgets/main_menu_drawer.dart';
 import '../../widgets/app_bar.dart';
+import '../../models/museum.dart';
+import '../../providers/museums.dart';
 
-class MuseumsOverviewScreen extends StatelessWidget {
+class MuseumsOverviewScreen extends StatefulWidget {
+  @override
+  State<MuseumsOverviewScreen> createState() => _MuseumsOverviewScreenState();
+}
+
+class _MuseumsOverviewScreenState extends State<MuseumsOverviewScreen> {
+  List<Museum> museums;
+  List<Museum> allMuseums;
+  String query = '';
+
+  @override
+  void didChangeDependencies() {
+    allMuseums = Provider.of<Museums>(context, listen: false).getMuseums;
+    museums = allMuseums;
+    super.didChangeDependencies();
+  }
+
   @override
   Widget build(BuildContext context) {
+    print('rebildam skrin');
     return Scaffold(
       appBar: appBar('Museum app', context, Theme.of(context).primaryColor),
       body: SingleChildScrollView(
@@ -18,14 +38,21 @@ class MuseumsOverviewScreen extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 DropDownCategory(),
-                SearchBar(),
+                SearchBar(query, searchMuseum),
               ],
             ),
-            MuseumsGrid(), //wrap with flexible if search is fixed
+            MuseumsGrid(museums), //wrap with flexible if search is fixed
           ],
         ),
       ),
       drawer: MainMenuDrawer(),
     );
+  }
+
+  void searchMuseum(String query) {
+    setState(() {
+      this.query = query;
+      this.museums = Provider.of<Museums>(context, listen:false).searchMuseums(query);
+    });
   }
 }
