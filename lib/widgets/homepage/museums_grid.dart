9472c945bdb62ad4e101eb35dfba4cdd214dd453
msakac <msakac@foi.hr>
2022-01-11 11:24:48 +0100

    Imported museum model. Added check if list of museum is empty. If true it shows image else it displays GridView of museums.

--- lib/widgets/homepage/museums_grid.dart
+++ lib/widgets/homepage/museums_grid.dart
@@ -3,20 +3,23 @@ import 'package:provider/provider.dart';
 
 import '../../providers/museums.dart';
 import './museum_grid_item.dart';
+import '../../models/museum.dart';
 
 
 class MuseumsGrid extends StatelessWidget {
+  List<Museum> museums;
+  MuseumsGrid(this.museums);
   @override
   Widget build(BuildContext context) {
-    final museumsData =
-        Provider.of<Museums>(context).getMuseums; //access to museums data
-    return GridView.builder(
+    print('rebilda grid muzeja');
+     //access to museums data
+    return museums.isNotEmpty ? GridView.builder(
       shrinkWrap: true, //remove this if search is fixed
       physics: const NeverScrollableScrollPhysics(),//remove this if search is fixed
       padding: const EdgeInsets.all(10),
-      itemCount: museumsData.length,
+      itemCount: museums.length,
       itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
-        value: museumsData[i],
+        value: museums[i],
         child: MuseumGridItem(),
       ),
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
@@ -24,6 +27,6 @@ class MuseumsGrid extends StatelessWidget {
         childAspectRatio: 3 / 2,
         mainAxisSpacing: 10,
       ),
-    );
+    ) : Padding(padding: EdgeInsets.only(top:20),child: Image.asset('assets/images/NoArtworks.png',fit: BoxFit.fill,));
   }
 }
