314c5a4488161e4f57106f244174f2d24bb3ce63
msakac <msakac@foi.hr>
2022-01-11 11:45:14 +0100

    Extracted widgets to Widget methods

--- lib/widgets/museumDetails/artwork_grid_item.dart
+++ lib/widgets/museumDetails/artwork_grid_item.dart
@@ -8,62 +8,71 @@ import '../../models/artwork.dart';
 class ArtworkGridItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    
     final artwork = Provider.of<Artwork>(context, listen: false);
 
     return ClipRRect(
       borderRadius: BorderRadius.circular(5),
       child: GridTile(
         child: Image.network(artwork.imageUrl, fit: BoxFit.cover),
-        header: Container(
-          //maybe have another screen where we display artwork info
-          padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                artwork.author,
-                softWrap: true,
-                style: TextStyle(
-                  color: Theme.of(context).highlightColor,
-                  backgroundColor: Colors.black87,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 13,
-                ),
-              ),
-              Consumer<Artwork>(
-                builder: (ctx, artwork, child) => IconButton(
-                  onPressed: () {
-                    artwork.toggleFavorite();
-                  },
-                  icon: Icon(
-                    artwork.isFavorite ? Icons.favorite : Icons.favorite_border,
-                    color: Theme.of(context).accentColor,
-                    size: 30,
-                  ),
-                ),
-              )
-            ],
+        header: buildHeader(context, artwork),
+        footer: buildFooter(context, artwork),
+      ),
+    );
+  }
+
+  Widget buildHeader(BuildContext context, Artwork artwork) {
+    return Container(
+      //maybe have another screen where we display artwork info
+      padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            artwork.author,
+            softWrap: true,
+            style: TextStyle(
+              color: Theme.of(context).highlightColor,
+              backgroundColor: Colors.black87,
+              fontWeight: FontWeight.bold,
+              fontSize: 13,
+            ),
           ),
-        ),
-        footer: Container(
-          padding: EdgeInsets.all(10),
-          color: Colors.black54,
-          child: Row(
-            children: [
-              Flexible(
-                child: Text(
-                  artwork.name,
-                  softWrap: true,
-                  style: TextStyle(
-                    color: Theme.of(context).highlightColor,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 15,
-                  ),
-                ),
+          Consumer<Artwork>(
+            builder: (ctx, artwork, child) => IconButton(
+              onPressed: () {
+                artwork.toggleFavorite();
+              },
+              icon: Icon(
+                artwork.isFavorite ? Icons.favorite : Icons.favorite_border,
+                color: Theme.of(context).accentColor,
+                size: 30,
               ),
-            ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget buildFooter(BuildContext context, Artwork artwork) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      color: Colors.black54,
+      child: Row(
+        children: [
+          Flexible(
+            child: Text(
+              artwork.name,
+              softWrap: true,
+              style: TextStyle(
+                color: Theme.of(context).highlightColor,
+                fontWeight: FontWeight.bold,
+                fontSize: 15,
+              ),
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
