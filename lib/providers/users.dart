ddfd86987cf3db6acfe8ac9c63553ebffcf73b87
Tom1sl4v97 <tomo.tomiek97@gmail.com>
2022-01-11 17:14:48 +0100

    Adding the methods in user provider

--- lib/providers/users.dart
+++ lib/providers/users.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: unused_field, prefer_final_fields
 
+import 'package:email_validator/email_validator.dart';
 import 'package:flutter/foundation.dart';
 import 'dart:convert';
 import 'package:crypto/crypto.dart';
@@ -53,5 +54,84 @@ class Users with ChangeNotifier {
   String useHash(String secret) {
     var salt = utf8.encode(secret);
     return sha256.convert(salt).toString();
+  }
+
+  bool isValidUsername(String username) {
+    if (username == null || username.isEmpty || usernameTaken(username)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  bool isValidEmail(String email) {
+    if (email == null || email.isEmpty || !EmailValidator.validate(email)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  bool usernameTaken(String username) {
+    if (_users.firstWhere((userData) => userData.username == username,
+            orElse: () => null) ==
+        null) {
+      return false;
+    }
+    return true;
+  }
+
+  bool isValidName(String name) {
+    if (name == null || name.isEmpty) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  bool isValidSurname(String surname) {
+    if (surname == null || surname.isEmpty) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  bool isValidPassword(String passOne, String passTwo) {
+    if (passOne == null ||
+        passTwo == null ||
+        passOne.isEmpty ||
+        passTwo.isEmpty) {
+      return true;
+    } else if (passOne == passTwo) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  void addNewUser(
+    String username,
+    String email,
+    String name,
+    String surname,
+    String pass,
+  ) {
+    var newId = (_users.length + 1).toString();
+    var salt = useHash(username);
+    var password = useHash(pass + salt);
+    _users.insert(
+      0,
+      User(
+        id: newId,
+        name: name,
+        surname: surname,
+        username: username,
+        email: email,
+        password: password,
+        salt: salt,
+        userRole: '0',
+      ),
+    );
   }
 }
