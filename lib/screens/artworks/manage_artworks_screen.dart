87de14da5f66f01be903a428de7ed40c808cd487
msakac <msakac@foi.hr>
2022-01-12 11:42:05 +0100

    Created ManageArtworksScreen. This screen is for museums admins. It displays all artworks. Artworks will be editable, deletable and addable. In ListView.builder we use ManageArtworkItem widget to display every list view tile with attributes and buttons.

--- lib/screens/artworks/manage_artworks_screen.dart
+++ lib/screens/artworks/manage_artworks_screen.dart
@@ -1,16 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../providers/artworks.dart';
 
 import '../../widgets/app_bar.dart';
 import '../../widgets/main_menu_drawer.dart';
-
+import '../../widgets/artworks/manage_artwork_item.dart';
 class ManageArtworksScreen extends StatelessWidget {
-  static const routeName ='/artworks';
+  static const routeName = '/artworks';
 
   @override
   Widget build(BuildContext context) {
+    ThemeData color = Theme.of(context);
+    final artworks = Provider.of<Artworks>(context).getArtworks;
+
     return Scaffold(
-      appBar: appBar('Artworks', context, Theme.of(context).primaryColor),
-      drawer: MainMenuDrawer()
-    );
+        appBar: appBar('Artworks', context, Theme.of(context).primaryColor),
+        body: ListView.builder(
+          itemCount: artworks.length,
+          itemBuilder: (_, i) =>Column(children: [
+            ManageArtworkItem(
+              artworks[i].id,
+              artworks[i].name,
+              artworks[i].imageUrl
+            ),
+            Divider(thickness: 1, color: color.highlightColor,)
+          ],),
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: color.highlightColor,
+          child: IconButton(
+            onPressed: () {
+              //TO DO - reroute to add/edit artwork
+            },
+            icon: Icon(
+              Icons.add,
+              size: 30,
+            ),
+          ),
+        ),
+        drawer: MainMenuDrawer());
   }
 }
