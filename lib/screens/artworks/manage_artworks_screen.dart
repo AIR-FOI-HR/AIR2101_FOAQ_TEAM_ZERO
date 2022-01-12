913e02f3dd4c7fda6545c7278dd64616203fcfb1
msakac <msakac@foi.hr>
2022-01-12 20:27:26 +0100

    Imported package grouped_list.dart, museums.dart provider and artwork.dart model. In body we are now using GroupedListView. This way we are splitting artworks in groups depending in which museum they belong.

--- lib/screens/artworks/manage_artworks_screen.dart
+++ lib/screens/artworks/manage_artworks_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:grouped_list/grouped_list.dart';
 
 import '../../providers/artworks.dart';
+import '../../providers/museums.dart';
+import '../../models/artwork.dart';
 
 import '../../widgets/app_bar.dart';
 import '../../widgets/main_menu_drawer.dart';
@@ -13,20 +16,57 @@ class ManageArtworksScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     ThemeData color = Theme.of(context);
     final artworks = Provider.of<Artworks>(context).getArtworks;
+    final museums = Provider.of<Museums>(context);
 
     return Scaffold(
         appBar: appBar('Artworks', context, Theme.of(context).primaryColor),
-        body: ListView.builder(
-          itemCount: artworks.length,
-          itemBuilder: (_, i) =>Column(children: [
-            ManageArtworkItem(
-              artworks[i].id,
-              artworks[i].name,
-              artworks[i].imageUrl
+         body: GroupedListView<Artwork, String>(
+          elements: artworks,
+          groupBy: (artwork) => artwork.museum,
+          groupSeparatorBuilder: (String groupByValue) => (Padding(
+            padding: EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  museums.getById(groupByValue).name,
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: color.primaryColor,
+                      fontWeight: FontWeight.bold),
+                ),
+                Divider(
+                  thickness: 2,
+                  color: color.highlightColor,
+                ),
+              ],
             ),
-            Divider(thickness: 1, color: color.highlightColor,)
-          ],),
+          )),
+          itemBuilder: (_, artwork) => Column(
+            children: [
+              ManageArtworkItem(
+                artwork.id,
+                artwork.name,
+                artwork.imageUrl,
+              ),
+              Divider(
+                thickness: 0.2,
+                color: Colors.black,
+              )
+            ],
+          ),
         ),
+         //ListView.builder(
+        //   itemCount: artworks.length,
+        //   itemBuilder: (_, i) =>Column(children: [
+        //     ManageArtworkItem(
+        //       artworks[i].id,
+        //       artworks[i].name,
+        //       artworks[i].imageUrl
+        //     ),
+        //     Divider(thickness: 1, color: color.highlightColor,)
+        //   ],),
+        // ),
         floatingActionButton: FloatingActionButton(
           backgroundColor: color.highlightColor,
           child: IconButton(
