30c9d061c999944bcc1d69c3f1dafda85830ebb1
msakac <msakac@foi.hr>
2022-01-13 21:09:01 +0100

    Added method addArtwork, updateArtwork and deleteArtwork. These methods() add, update and delete data from the database. Also they add, update and delete local data.

--- lib/providers/artworks.dart
+++ lib/providers/artworks.dart
@@ -5,73 +5,110 @@ import '../models/artwork.dart';
 import '../models/museum.dart';
 import '../providers/museums.dart';
 
-
 class Artworks with ChangeNotifier {
-
-  final urlArtworks = Uri.parse('https://museumapp-3725f-default-rtdb.europe-west1.firebasedatabase.app/artworks.json');
-
+  final urlArtworks = Uri.parse(
+      'https://museumapp-3725f-default-rtdb.europe-west1.firebasedatabase.app/artworks.json');
   List<Artwork> _artworks = [];
-  
-  List<Artwork> get getArtworks{
+
+  List<Artwork> get getArtworks {
     return [..._artworks];
   }
 
-  Future<void> fetchAndSetArtwotks() async{
-    try{
+  Future<void> fetchAndSetArtwotks() async {
+    try {
       final response = await http.get(urlArtworks);
       final extracedData = json.decode(response.body) as Map<String, dynamic>;
       final List<Artwork> loadedArtworks = [];
-      print('Baza: '+ extracedData.length.toString());
-      print('Lokalno: '+ _artworks.length.toString());
-      if(extracedData.length == _artworks.length){
+      print('Baza: ' + extracedData.length.toString());
+      print('Lokalno: ' + _artworks.length.toString());
+      if (extracedData.length == _artworks.length) {
         return;
       }
-      extracedData.forEach((id, artwork) { 
+      extracedData.forEach((id, artwork) {
         print('Dohvacam artwork');
         loadedArtworks.add(Artwork(
-          id: id,
-          category: artwork['category'],
-          imageUrl: artwork['imageUrl'],
-          museum: artwork['museum'],
-          name: artwork['name'],
-          author: artwork['author'],
-          description: artwork['description'],
-          isFavorite: false
-        ));
+            id: id,
+            category: artwork['category'],
+            imageUrl: artwork['imageUrl'],
+            museum: artwork['museum'],
+            name: artwork['name'],
+            author: artwork['author'],
+            description: artwork['description'],
+            isFavorite: false));
       });
       _artworks = loadedArtworks;
-    }catch(error){
-      throw(error);
+    } catch (error) {
+      throw (error);
     }
   }
-  
+
   List<Artwork> getByMuseumId(String id) {
     return _artworks.where((artwork) => artwork.museum == id).toList();
   }
 
-  Artwork getById(String id){
+  Artwork getById(String id) {
     return _artworks.firstWhere((artwork) => artwork.id == id);
   }
 
-  List<Artwork> getByCategory(String categoryId){
-    return _artworks.where((artwork) => artwork.category == categoryId).toList();
+  List<Artwork> getByCategory(String categoryId) {
+    return _artworks
+        .where((artwork) => artwork.category == categoryId)
+        .toList();
   }
 
-//   Future<void> addArtworks() async{
-//     for (Artwork artwork in _artworks) {
-//       try{
-//         await http.post(urlArtworks, body: json.encode({
-//           'name': artwork.name,
-//           'imageUrl':artwork.imageUrl,
-//           'description':artwork.description,
-//           'author':artwork.author,
-//           'museum':artwork.museum,
-//           'category':artwork.category
-//         }));
-//       }catch(error){
-//         print(error);
-//       }
-//     }
-//     print('Uspjesno prebaceni podaci u bazu');
-//   }
+  Future<void> addArtwork(Artwork artwork) async {
+    try {
+      final response = await http.post(urlArtworks,
+          body: json.encode({
+            'name': artwork.name,
+            'imageUrl': artwork.imageUrl,
+            'description': artwork.description,
+            'author': artwork.author,
+            'museum': artwork.museum,
+            'category': artwork.category
+          }));
+
+      final newArtwork = Artwork(
+        category: artwork.category,
+        museum: artwork.museum,
+        name: artwork.name,
+        author: artwork.author,
+        description: artwork.description,
+        imageUrl: artwork.imageUrl,
+        id: json.decode(response.body)['name'],
+      );
+
+      print('New artwork: ' + json.decode(response.body)['name']);
+      _artworks.add(newArtwork);
+      notifyListeners();
+    } catch (error) {
+      print(error);
+    }
+  }
+
+  Future<void> updateArtwork(String id, Artwork newArtwork) async {
+    final artworkIndex = _artworks.indexWhere((artwork) => artwork.id == id);
+    if (artworkIndex >= 0) {
+      final url = Uri.parse(
+          'https://museumapp-3725f-default-rtdb.europe-west1.firebasedatabase.app/artworks/$id.json');
+      await http.patch(url, body: json.encode({
+        'author': newArtwork.author,
+        'category' : newArtwork.category,
+        'description': newArtwork.description,
+        'imageUrl':newArtwork.imageUrl,
+        'museum':newArtwork.museum,
+        'name':newArtwork.name
+      }));
+      _artworks[artworkIndex] = newArtwork;
+      notifyListeners();
+    }else{}
+  }
+
+  Future<void> deleteArtwork(String id) {
+    //to do -> optimistic deletion and error handling
+      final url = Uri.parse(
+          'https://museumapp-3725f-default-rtdb.europe-west1.firebasedatabase.app/artworks/$id.json');
+      http.delete(url);
+      notifyListeners();
+  }
 }
