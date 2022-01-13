0f3f5eb94f586f56e2aa8fa6a7fefec8982105ca
Tom1sl4v97 <tomo.tomiek97@gmail.com>
2022-01-13 13:20:59 +0100

    Changing the height to dynamic height

--- lib/screens/my_profile/my_profile_screen.dart
+++ lib/screens/my_profile/my_profile_screen.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
 import '../../widgets/app_bar.dart';
 import '../../widgets/main_menu_drawer.dart';
+import '../../widgets/my_profile/user_picture.dart';
 
 class MyProfileScreen extends StatelessWidget {
   static const routeName = '/myProfile';
   @override
   Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context);
     final appBarProperty =
         appBar('My profile', context, Theme.of(context).primaryColor);
     return Scaffold(
       appBar: appBarProperty,
       drawer: MainMenuDrawer(),
       body: Column(
-        children: [],
+        children: [
+          Container(
+              height: (mediaQuery.size.height -
+                      appBarProperty.preferredSize.height -
+                      mediaQuery.padding.top) *
+                  0.4,
+              child: UserPicture()),
+        ],
       ),
     );
   }
