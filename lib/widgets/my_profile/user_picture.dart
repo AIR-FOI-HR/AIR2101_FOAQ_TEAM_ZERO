3f6baaa9924417f0f3040283f34051f0dabc7d70
Tom1sl4v97 <tomo.tomiek97@gmail.com>
2022-01-13 13:21:25 +0100

    Creating the image design for the my profile screen

--- lib/widgets/my_profile/user_picture.dart
+++ lib/widgets/my_profile/user_picture.dart
@@ -4,16 +4,52 @@ import 'package:provider/provider.dart';
 import '../../providers/users.dart';
 
 class UserPicture extends StatelessWidget {
-  final String user = 'msakac';
+  final String user = 'ttomiek';
 
   @override
   Widget build(BuildContext context) {
-    final userData = Provider.of<Users>(context);
+    final users = Provider.of<Users>(context);
+    final userData = users.findByUsername(user);
+
     return Container(
-      child: Column(
-        children: [
-          Image.network(userData.findByUsername(user).userImage),
-        ],
+      margin: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30),
+        color: Theme.of(context).primaryColor,
+      ),
+      width: double.infinity,
+      child: LayoutBuilder(
+        builder: (ctx, constraints) {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: constraints.maxHeight * 0.08,
+              ),
+              Container(
+                width: constraints.maxWidth * 0.4,
+                height: constraints.maxHeight * 0.6,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    fit: BoxFit.fill,
+                    image: userData.userImage == null
+                        ? const AssetImage('assets/images/default_user.png')
+                        : NetworkImage(userData.userImage),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                userData.username,
+                style: Theme.of(context).textTheme.headline6,
+              )
+            ],
+          );
+        },
       ),
     );
   }
