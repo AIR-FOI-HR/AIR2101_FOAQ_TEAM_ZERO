d289bc79aca689d33816699a0d74121eb043dddc
msakac <msakac@foi.hr>
2022-01-13 18:00:46 +0100

    Added DropDownFields for Museum and Category. We get museums and category with provider and convert them to list so we can connect them with the dropdown menu. Added FloatingActionButton to save changes.

--- lib/screens/artworks/edit_add_artworks_screen.dart
+++ lib/screens/artworks/edit_add_artworks_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
+import 'package:dropdownfield/dropdownfield.dart';
 
 import '../../widgets/app_bar.dart';
 import '../../models/artwork.dart';
 import '../../providers/artworks.dart';
+import '../../providers/museums.dart';
+import '../../providers/categories.dart';
 
 class EditAddArtworksScreen extends StatefulWidget {
   static const routeName = '/add-edit-artworks';
@@ -25,10 +28,16 @@ class _EditAddArtworksScreenState extends State<EditAddArtworksScreen> {
       description: '');
   final _formKey = GlobalKey<FormState>();
 
+  final selectedMuseum = TextEditingController();
+  String museum = "";
+
+  final selectedCategory = TextEditingController();
+  String category = "";
   @override
   void didChangeDependencies() {
     if (_isInit) {
       final artworkId = ModalRoute.of(context).settings.arguments as String;
+      print(artworkId);
       if (artworkId != null) {
         _artwork = Provider.of<Artworks>(context).getById(artworkId);
       }
@@ -40,63 +49,131 @@ class _EditAddArtworksScreenState extends State<EditAddArtworksScreen> {
   @override
   Widget build(BuildContext context) {
     ThemeData color = Theme.of(context);
+
+    Map<String, String> museumsMap = {};
+    Provider.of<Museums>(context)
+        .getMuseums
+        .forEach((museum) => museumsMap[museum.id] = museum.name);
+
+    Map<String, String> categoryMap = {};
+    Provider.of<Categories>(context)
+        .items
+        .forEach((category) => categoryMap[category.id] = category.name);
+
     return Scaffold(
-        appBar: appBar(
-          _artwork.id != null ? _artwork.name : 'Add artwork',
-          context,
-          color.primaryColor,
-        ),
-        body: Padding(
-          padding: EdgeInsets.all(20),
-          child: Form(
-              key: _formKey,
-              child: ListView(children: [
-                TextFormField(
-                  decoration: inputDecoration('Name', Icons.text_fields, color),
-                ),
-                SizedBox(height: 10),
-                TextFormField(
-                  decoration: inputDecoration('Author', Icons.person, color),
-                  
-                ),
-                SizedBox(height: 10),
-                TextFormField(
-                  decoration:
-                      inputDecoration('Description', Icons.description, color),
-                  maxLines: 4,
-                  keyboardType: TextInputType.multiline,
-                ),
-                SizedBox(height: 10),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Container(
-                      width: 150,
-                      height: 150,
-                      margin: EdgeInsets.only(
-                        top: 8,
-                        right: 10,
-                      ),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          width: 1,
-                          color: color.highlightColor,
-                        ),
-                      ),
-                      child: FittedBox(
-                        child: Image.asset('assets/images/NoArtworks.png'),
-                        fit: BoxFit.contain,
+      appBar: appBar(
+        _artwork.id != null ? _artwork.name : 'Add artwork',
+        context,
+        color.primaryColor,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: [
+              TextFormField(
+                decoration: inputDecoration('Name', Icons.text_fields, color),
+              ),
+              SizedBox(height: 10),
+              DropDownField(
+                controller: selectedMuseum,
+                textStyle: TextStyle(fontSize: 16, color: color.primaryColor),
+                labelStyle: TextStyle(fontSize: 16, color: color.primaryColor),
+                icon: Icon(Icons.museum, color: color.primaryColor),
+                enabled: true,
+                labelText: 'Museum',
+                items: museumsMap.values.toList(),
+                onValueChanged: (value) {
+                  setState(() {
+                    museum = museumsMap.keys
+                        .firstWhere((key) => museumsMap[key] == value);
+                    print(museum);
+                  });
+                },
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              DropDownField(
+                controller: selectedCategory,
+                textStyle: TextStyle(fontSize: 16, color: color.primaryColor),
+                labelStyle: TextStyle(fontSize: 16, color: color.primaryColor),
+                icon: Icon(Icons.category, color: color.primaryColor),
+                labelText: 'Category',
+                enabled: true,
+                items: categoryMap.values.toList(),
+                onValueChanged: (value) {
+                  setState(() {
+                    category = categoryMap.keys
+                        .firstWhere((key) => categoryMap[key] == value);
+                    print(category);
+                  });
+                },
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                decoration: inputDecoration('Author', Icons.person, color),
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                decoration:
+                    inputDecoration('Description', Icons.description, color),
+                maxLines: 4,
+                keyboardType: TextInputType.multiline,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    width: 150,
+                    height: 150,
+                    margin: EdgeInsets.only(
+                      top: 8,
+                      right: 10,
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        width: 1,
+                        color: color.highlightColor,
                       ),
                     ),
-                    Expanded(
-                        child: TextFormField(
-                      decoration: inputDecoration('Image URL', Icons.image, color),
-                      keyboardType: TextInputType.url,
-                    ))
-                  ],
-                ),
-              ])),
-        ));
+                    child: FittedBox(
+                      child: Image.asset('assets/images/NoArtworks.png'),
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                  Expanded(
+                      child: TextFormField(
+                    decoration:
+                        inputDecoration('Image URL', Icons.image, color),
+                    keyboardType: TextInputType.url,
+                  ))
+                ],
+              ),
+            
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: color.highlightColor,
+        onPressed: () {
+          
+        },
+        child: IconButton(
+          icon: Icon(
+            Icons.check,
+            color: color.primaryColor,
+            size: 35,
+          ),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+    );
   }
 
   InputDecoration inputDecoration(
