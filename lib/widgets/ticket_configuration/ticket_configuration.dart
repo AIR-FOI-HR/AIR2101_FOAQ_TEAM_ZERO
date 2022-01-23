ced612c1248e2352785b1c91415b6e8829317bdb
Tom1sl4v97 <tomo.tomiek97@gmail.com>
2022-01-23 18:36:38 +0100

    Changing the witget ticket configuration to have a dynamic height

--- lib/widgets/ticket_configuration/ticket_configuration.dart
+++ lib/widgets/ticket_configuration/ticket_configuration.dart
@@ -12,36 +12,38 @@ class TicketConfiguration extends StatelessWidget {
   Widget build(BuildContext context) {
     final color = Theme.of(context);
     final ticketListData = Provider.of<Tickets>(context).getTickets(muesumId);
-    return SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8),
-            child: Text(
-              'Tickets',
-              style: color.textTheme.headline5,
+    return LayoutBuilder(
+      builder: (ctx, constraints) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Text(
+                'Tickets',
+                style: color.textTheme.headline5,
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 20, top: 10),
-            child: SizedBox(
-              height: 100,
-              child: ListView.builder(
-                itemCount: ticketListData.length,
-                itemBuilder: (_, i) => Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                        'name: ${ticketListData[i].name} cost: ${ticketListData[i].cost}'),
-                    Divider(),
-                  ],
+            SizedBox(
+              height: constraints.maxHeight * 0.65,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20, top: 10),
+                child: ListView.builder(
+                  itemCount: ticketListData.length,
+                  itemBuilder: (_, i) => Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                          'name: ${ticketListData[i].name} cost: ${ticketListData[i].cost}'),
+                      Divider(),
+                    ],
+                  ),
                 ),
               ),
             ),
-          )
-        ],
-      ),
+          ],
+        );
+      },
     );
   }
 }
