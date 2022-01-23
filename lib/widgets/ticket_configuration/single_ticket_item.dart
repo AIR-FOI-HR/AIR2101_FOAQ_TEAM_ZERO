38febf239212ae48dd445421a4938b63dffba9f9
Tom1sl4v97 <tomo.tomiek97@gmail.com>
2022-01-23 19:07:09 +0100

    Creating the row widget for displaying the ticket data and icon buton for editing

---
+++ lib/widgets/ticket_configuration/single_ticket_item.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import '../../models/ticket.dart';
+
+class SingleTicketItem extends StatelessWidget {
+  final Ticket ticketData;
+
+  SingleTicketItem(this.ticketData);
+
+  @override
+  Widget build(BuildContext context) {
+    final color = Theme.of(context);
+    return Column(
+      children: [
+        Row(
+          children: [
+            Expanded(
+              flex: 5,
+              child: Text(
+                ticketData.name,
+                style: const TextStyle(fontSize: 16),
+              ),
+            ),
+            Expanded(
+              child: Text(
+                '${ticketData.cost}€',
+                style: const TextStyle(fontSize: 16),
+                textAlign: TextAlign.right,
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: SizedBox(
+                height: 30,
+                child: IconButton(
+                  padding: const EdgeInsets.all(0),
+                  onPressed: () {},
+                  icon: const Icon(Icons.settings),
+                ),
+              ),
+            )
+          ],
+        ),
+        const Divider(),
+      ],
+    );
+  }
+}
