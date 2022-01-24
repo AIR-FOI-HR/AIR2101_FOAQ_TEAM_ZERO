c66d9ca33adc884de2f4ab7b0b2a048323640d8f
Tom1sl4v97 <tomo.tomiek97@gmail.com>
2022-01-24 16:32:40 +0100

    Removing the print of testing

--- lib/widgets/homepage/museums_grid.dart
+++ lib/widgets/homepage/museums_grid.dart
@@ -5,28 +5,34 @@ import '../../providers/museums.dart';
 import './museum_grid_item.dart';
 import '../../models/museum.dart';
 
-
 class MuseumsGrid extends StatelessWidget {
   List<Museum> museums;
   MuseumsGrid(this.museums);
   @override
   Widget build(BuildContext context) {
-    print('rebilda grid muzeja');
-     //access to museums data
-    return museums.isNotEmpty ? GridView.builder(
-      shrinkWrap: true, //remove this if search is fixed
-      physics: const NeverScrollableScrollPhysics(),//remove this if search is fixed
-      padding: const EdgeInsets.all(10),
-      itemCount: museums.length,
-      itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
-        value: museums[i],
-        child: MuseumGridItem(),
-      ),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 1,
-        childAspectRatio: 3 / 2,
-        mainAxisSpacing: 10,
-      ),
-    ) : Padding(padding: EdgeInsets.only(top:20),child: Image.asset('assets/images/NoArtworks.png',fit: BoxFit.fill,));
+    //access to museums data
+    return museums.isNotEmpty
+        ? GridView.builder(
+            shrinkWrap: true, //remove this if search is fixed
+            physics:
+                const NeverScrollableScrollPhysics(), //remove this if search is fixed
+            padding: const EdgeInsets.all(10),
+            itemCount: museums.length,
+            itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
+              value: museums[i],
+              child: MuseumGridItem(),
+            ),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 1,
+              childAspectRatio: 3 / 2,
+              mainAxisSpacing: 10,
+            ),
+          )
+        : Padding(
+            padding: EdgeInsets.only(top: 20),
+            child: Image.asset(
+              'assets/images/NoArtworks.png',
+              fit: BoxFit.fill,
+            ));
   }
 }
