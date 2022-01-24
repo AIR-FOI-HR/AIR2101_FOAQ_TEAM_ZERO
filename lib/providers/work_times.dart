083b6107e2da58b9b96355ea682b9ee4d8a15278
Tom1sl4v97 <tomo.tomiek97@gmail.com>
2022-01-24 13:59:35 +0100

    Chaning some test dummy data and creting thet if museum dosent have a workt time scadule jet then it create the epmty work time list that contain the day name but not the time (time will display thet it is closed)

--- lib/providers/work_times.dart
+++ lib/providers/work_times.dart
@@ -1,95 +1,85 @@
 // ignore_for_file: prefer_final_fields
 
+import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
 import '../models/work_time.dart';
 
 class WorkTimes with ChangeNotifier {
   List<WorkTime> _workTimes = [
     WorkTime(
-      id: '1',
+      id: '0',
       day: 'Monday',
+      timeFrom: const TimeOfDay(hour: 8, minute: 0),
+      timeTo: const TimeOfDay(hour: 16, minute: 0),
+      museumId: '1',
+    ),
+    WorkTime(
+      id: '1',
+      day: 'Tuesday',
       timeFrom: const TimeOfDay(hour: 8, minute: 0),
       timeTo: const TimeOfDay(hour: 16, minute: 0),
       museumId: '1',
     ),
     WorkTime(
       id: '2',
-      day: 'Tuesday',
+      day: 'Wednesday',
       timeFrom: const TimeOfDay(hour: 8, minute: 0),
       timeTo: const TimeOfDay(hour: 16, minute: 0),
       museumId: '1',
     ),
     WorkTime(
       id: '3',
-      day: 'Wednesday',
+      day: 'Thursday',
       timeFrom: const TimeOfDay(hour: 8, minute: 0),
       timeTo: const TimeOfDay(hour: 16, minute: 0),
       museumId: '1',
     ),
     WorkTime(
       id: '4',
-      day: 'Thursday',
-      timeFrom: const TimeOfDay(hour: 8, minute: 0),
-      timeTo: const TimeOfDay(hour: 16, minute: 0),
+      day: 'Friday',
       museumId: '1',
     ),
     WorkTime(
       id: '5',
-      day: 'Friday',
+      day: 'Saturday',
+      timeFrom: const TimeOfDay(hour: 8, minute: 0),
+      timeTo: const TimeOfDay(hour: 20, minute: 0),
       museumId: '1',
     ),
     WorkTime(
       id: '6',
-      day: 'Saturday',
+      day: 'Sunday',
       timeFrom: const TimeOfDay(hour: 8, minute: 0),
       timeTo: const TimeOfDay(hour: 20, minute: 0),
       museumId: '1',
-    ),
-    WorkTime(
-      id: '7',
-      day: 'Nedelja',
-      timeFrom: const TimeOfDay(hour: 8, minute: 0),
-      timeTo: const TimeOfDay(hour: 20, minute: 0),
-      museumId: '1',
-    ),
-    WorkTime(
-      id: '8',
-      day: 'Monday',
-      museumId: '2',
-    ),
-    WorkTime(
-      id: '9',
-      day: 'Tuesday',
-      museumId: '2',
-    ),
-    WorkTime(
-      id: '10',
-      day: 'Wednesday',
-      museumId: '2',
-    ),
-    WorkTime(
-      id: '11',
-      day: 'Thursday',
-      museumId: '2',
-    ),
-    WorkTime(
-      id: '12',
-      day: 'Friday',
-      museumId: '1',
-    ),
-    WorkTime(
-      id: '13',
-      day: 'Saturday',
-      museumId: '2',
-    ),
-    WorkTime(
-      id: '14',
-      day: 'Nedelja',
-      museumId: '2',
     ),
   ];
 
+  List days = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday',
+  ];
+
   List<WorkTime> getWorkTime(String id) {
+    final workTimeList = _workTimes.firstWhere(
+        (workTimeData) => workTimeData.museumId == id,
+        orElse: () => null);
+    if (workTimeList == null) {
+      int theLastId = _workTimes.length;
+      for (int i = 0; i < 7; i++) {
+        final newWorkTime = WorkTime(
+          id: (theLastId + i).toString(),
+          day: days[i],
+          museumId: id,
+        );
+        _workTimes.add(newWorkTime);
+      }
+    }
     return _workTimes
         .where((workTimeData) => workTimeData.museumId == id)
         .toList();
