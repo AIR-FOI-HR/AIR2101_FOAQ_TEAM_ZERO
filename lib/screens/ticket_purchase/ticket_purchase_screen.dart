250f2118f503af0f8d72a56e004f560b58d54b5a
Tom1sl4v97 <tomo.tomiek97@gmail.com>
2022-01-26 21:49:55 +0100

    Creating the new widget for displaying the buy tickets

--- lib/screens/ticket_purchase/ticket_purchase_screen.dart
+++ lib/screens/ticket_purchase/ticket_purchase_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../widgets/main_menu_drawer.dart';
+import '../../widgets/ticket_purchase/buy_ticket.dart';
 
 class TicketPurchaseScreen extends StatelessWidget {
   static const routeName = '/TicketPurchase';
@@ -8,26 +9,29 @@ class TicketPurchaseScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final color = Theme.of(context);
+
+    final appBarProperty = AppBar(
+      title: const Text('Tickets'),
+      backgroundColor: color.primaryColor,
+      bottom: TabBar(
+        indicatorColor: color.highlightColor,
+        labelStyle: color.textTheme.headline4,
+        tabs: const [
+          Tab(text: 'Buy tickets'),
+          Tab(text: 'My reservations'),
+        ],
+      ),
+    );
+
     return DefaultTabController(
       length: 2,
       child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Tickets'),
-          backgroundColor: color.primaryColor,
-          bottom: TabBar(
-            indicatorColor: color.highlightColor,
-            labelStyle: color.textTheme.headline4,
-            tabs: const [
-              Tab(text: 'Buy tickets'),
-              Tab(text: 'My reservations'),
-            ],
-          ),
-        ),
+        appBar: appBarProperty,
         drawer: MainMenuDrawer(),
         body: TabBarView(
           children: [
-            Text('DOGS'),
-            Text('CATS'),
+            BuyTicket(),
+            Text('Todo'),
           ],
         ),
       ),
---
+++ lib/widgets/ticket_purchase/buy_ticket.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import '../homepage/search_bar.dart';
+
+class BuyTicket extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      child: Text('data'),
+    );
+  }
+}
