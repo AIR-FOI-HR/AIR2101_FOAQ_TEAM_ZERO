635e6872e135f9ae9f2fb3491cbc7e99fa288400
Tom1sl4v97 <tomo.tomiek97@gmail.com>
2022-01-26 22:24:59 +0100

    Displaying the data for all the museums

--- lib/widgets/ticket_purchase/buy_ticket.dart
+++ lib/widgets/ticket_purchase/buy_ticket.dart
@@ -5,6 +5,7 @@ import '../homepage/search_bar.dart';
 import '../../models/museum.dart';
 import '../../providers/museums.dart';
 import '../../widgets/homepage/dropdown_category.dart';
+import './buy_ticket_single_museum.dart';
 
 class BuyTicket extends StatefulWidget {
   @override
@@ -46,20 +47,46 @@ class _BuyTicketState extends State<BuyTicket> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(10),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              DropDownCategory(searchMuseumByCategory),
-              SearchBar(searchMuseum),
-            ],
+    return LayoutBuilder(
+      builder: (ctx, constraints) {
+        return SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    DropDownCategory(searchMuseumByCategory),
+                    SearchBar(searchMuseum),
+                  ],
+                ),
+                SizedBox(height: constraints.maxHeight * 0.03),
+                museumsForWidget.isEmpty
+                    ? Padding(
+                        padding: EdgeInsets.only(top: 20),
+                        child: Image.asset(
+                          'assets/images/NoArtworks.png',
+                          fit: BoxFit.fill,
+                        ))
+                    : SizedBox(
+                        height: constraints.maxHeight * 0.82,
+                        child: ListView.builder(
+                          itemCount: museumsForWidget.length,
+                          itemBuilder: (_, i) => Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              BuyTicketSingleMuseum(museumsForWidget[i]),
+                            ],
+                          ),
+                        ),
+                      ),
+              ],
+            ),
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
