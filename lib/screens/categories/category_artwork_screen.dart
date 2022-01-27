ed126b3eee87642938e035eae3309aa645cff4b4
msakac <msakac@foi.hr>
2022-01-27 20:15:21 +0100

    Converted to stateful widget. Added FutureBuilder widget that executes method _fetchCategories. While this method is running we show progresIndicator. When we fetch all the categories, indicator is gone.

--- lib/screens/categories/category_artwork_screen.dart
+++ lib/screens/categories/category_artwork_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:museum_app/models/category_artwork.dart';
+import 'package:museum_app/providers/categories.dart';
 import '../../widgets/app_bar.dart';
 import '../../widgets/main_menu_drawer.dart';
 import '../../widgets/categories/showing/category_artwork_grid.dart';
@@ -8,14 +10,23 @@ import '../../providers/users.dart';
 import '../../models/user.dart';
 import 'package:provider/provider.dart';
 
-
 //tomekovo
-class CategoryArtworkScreen extends StatelessWidget {
+class CategoryArtworkScreen extends StatefulWidget {
   static const routeName = '/categoryArtwork';
 
   @override
+  State<CategoryArtworkScreen> createState() => _CategoryArtworkScreenState();
+}
+
+class _CategoryArtworkScreenState extends State<CategoryArtworkScreen> {
+  @override
   Widget build(BuildContext context) {
     User appUser = Provider.of<Users>(context).getUser();
+
+    Future<void> _fetchCategories() async {
+      await Provider.of<Categories>(context, listen: false).fetchCategories();
+    }
+
     const int privileges = 0;
     final appBarProperty =
         appBar('Categories', context, Theme.of(context).primaryColor, appUser);
@@ -23,22 +34,34 @@ class CategoryArtworkScreen extends StatelessWidget {
     return Scaffold(
       appBar: appBarProperty,
       drawer: MainMenuDrawer(),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(
-              height: (mediaQuery.size.height -
-                      appBarProperty.preferredSize.height -
-                      mediaQuery.padding.top) *
-                  0.87,
-              child: CategoryArtworkGrid(),
-            ),
-            Divider(
-              thickness: 2,
-              color: Theme.of(context).primaryColor,
-            ),
-            privileges == 1 ? AddNewCategoryButton() : SaveButton(),
-          ],
+      body: Container(
+        child: FutureBuilder(
+          future: _fetchCategories(),
+          builder: (ctx, snapshot) =>
+              snapshot.connectionState == ConnectionState.waiting
+                  ? Center(child: CircularProgressIndicator())
+                  : SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            height: (mediaQuery.size.height -
+                                    appBarProperty.preferredSize.height -
+                                    mediaQuery.padding.top) *
+                                0.87,
+                            child: CategoryArtworkGrid(),
+                          ),
+                          Divider(
+                            thickness: 2,
+                            color: Theme.of(context).primaryColor,
+                          ),
+                          appUser == null
+                              ? SaveButton()
+                              : int.parse(appUser.userRole) > 2
+                                  ? AddNewCategoryButton()
+                                  : SaveButton(),
+                        ],
+                      ),
+                    ),
         ),
       ),
     );
