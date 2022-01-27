c01b2797f7b61dad6cce37398280ff590e75fae6
Tom1sl4v97 <tomo.tomiek97@gmail.com>
2022-01-27 15:52:34 +0100

    Adding the atribut capacity of a museum

--- lib/models/museum.dart
+++ lib/models/museum.dart
@@ -8,6 +8,7 @@ class Museum with ChangeNotifier {
   final double tourDuration;
   final String imageUrl;
   final String location;
+  final int capacity;
 
   //only id and name are required. If sys admin creates new museum he only has to give museum the name.
   //museum admin can then add other info about the museum
@@ -19,5 +20,6 @@ class Museum with ChangeNotifier {
     this.tourDuration,
     this.imageUrl,
     this.location,
+    this.capacity,
   });
 }
