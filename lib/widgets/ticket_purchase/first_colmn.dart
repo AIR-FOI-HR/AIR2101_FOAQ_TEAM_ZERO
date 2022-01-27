d723e7e4eaf4ce4600e469b749c2c47199f08fa1
Tom1sl4v97 <tomo.tomiek97@gmail.com>
2022-01-27 16:01:08 +0100

    refactoring the code, it display the first column of information in the buy_ticket_single_museum.dart file

---
+++ lib/widgets/ticket_purchase/first_colmn.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../models/museum.dart';
+import '../../providers/work_times.dart';
+
+class FirstColumn extends StatelessWidget {
+  final Museum museumData;
+
+  FirstColumn(this.museumData);
+
+  @override
+  Widget build(BuildContext context) {
+    final color = Theme.of(context);
+    return Expanded(
+      flex: 3,
+      child: Padding(
+        padding: const EdgeInsets.all(3.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            FittedBox(
+              fit: BoxFit.cover,
+              child: Text(
+                museumData.name,
+                style: color.textTheme.headline5,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              museumData.address,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+            ),
+            const SizedBox(height: 10),
+            Text(
+              Provider.of<WorkTimes>(context)
+                  .getTheWorkTime(museumData.id, context),
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
