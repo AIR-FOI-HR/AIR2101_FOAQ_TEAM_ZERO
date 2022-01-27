d79d10f092546b8222c1c200315f6394de8df75e
msakac <msakac@foi.hr>
2022-01-27 20:13:20 +0100

    Added method fromSnap that converts json to category object. Added method toJson that converts object to json format for database.

--- lib/models/category_artwork.dart
+++ lib/models/category_artwork.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 
 class CategoryArtwork {
@@ -5,7 +6,17 @@ class CategoryArtwork {
   final String name;
 
   CategoryArtwork({
-    @required this.id,
+    this.id,
     @required this.name,
   });
+
+  static CategoryArtwork fromSnap(DocumentSnapshot snap) {
+    var snapshot = snap.data() as Map<String, String>;
+    return CategoryArtwork(
+      id: snap.id,
+      name: snap["name"],
+    );
+  }
+
+  Map<String, dynamic> toJson() => {"name": name};
 }
