f572f98e0ec4ec80f48c71b8f145ebc93a2748e3
msakac <msakac@foi.hr>
2022-01-27 23:25:50 +0100

    Added changeNotifier

--- lib/models/category_artwork.dart
+++ lib/models/category_artwork.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 
-class CategoryArtwork {
+class CategoryArtwork with ChangeNotifier{
   final String id;
   final String name;
 
