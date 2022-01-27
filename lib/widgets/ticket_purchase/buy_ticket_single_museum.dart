db63173a0e0314d5fcba537017e3de9a01d77752
Tom1sl4v97 <tomo.tomiek97@gmail.com>
2022-01-27 15:59:23 +0100

    Creating the widget for displaying the data of specific museum

--- lib/widgets/ticket_purchase/buy_ticket_single_museum.dart
+++ lib/widgets/ticket_purchase/buy_ticket_single_museum.dart
@@ -1,17 +1,60 @@
 import 'package:flutter/material.dart';
 
 import '../../models/museum.dart';
+import './museum_image.dart';
+import './first_colmn.dart';
 
 class BuyTicketSingleMuseum extends StatelessWidget {
   final Museum museumData;
+  final int index;
 
-  BuyTicketSingleMuseum(this.museumData);
+  BuyTicketSingleMuseum(this.museumData, this.index);
+
+  Widget itemDesign(BuildContext context, bool side) {
+    final color = Theme.of(context);
+    return SizedBox(
+      height: 150,
+      child: Card(
+        color: Colors.white,
+        elevation: 4,
+        child: Row(
+          children: [
+            if (side) MuseumImage(museumData.imageUrl),
+            FirstColumn(museumData),
+            Expanded(
+              flex: 2,
+              child: Column(
+                children: [
+                  FittedBox(
+                    fit: BoxFit.cover,
+                    child: Text(
+                      museumData.name,
+                      style: color.textTheme.headline5,
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  FittedBox(
+                    fit: BoxFit.cover,
+                    child: Text(
+                      museumData.address,
+                      style: color.textTheme.headline4,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            if (!side) MuseumImage(museumData.imageUrl),
+          ],
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 200,
-      child: Text(museumData.name),
-    );
+    final color = Theme.of(context);
+    return ((index % 2 == 0) || (index == 0))
+        ? itemDesign(context, true)
+        : itemDesign(context, false);
   }
 }
