69189073bf5d05432150d7d625551baf82199928
Tom1sl4v97 <tomo.tomiek97@gmail.com>
2022-01-28 17:23:33 +0100

    Adding the buy ticket button

--- lib/widgets/ticket_purchase/first_colmn.dart
+++ lib/widgets/ticket_purchase/first_colmn.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../../models/museum.dart';
 import '../../providers/work_times.dart';
+import '../ticket_configuration/elevated_button_settings.dart';
 
 class FirstColumn extends StatelessWidget {
   final Museum museumData;
@@ -33,14 +34,31 @@ class FirstColumn extends StatelessWidget {
           Text(
             Provider.of<WorkTimes>(context)
                 .getTheWorkTime(museumData.id, context),
-            maxLines: 2,
-            overflow: TextOverflow.ellipsis,
           ),
           const SizedBox(height: 10),
-          const Text(
-            'Category',
-            maxLines: 2,
-            overflow: TextOverflow.ellipsis,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                flex: 6,
+                child: Text(
+                  'categories categories categories categories categories categories ',
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.shopping_basket_outlined,
+                    size: 30,
+                    color: color.primaryColor,
+                  ),
+                ),
+              ),
+            ],
           ),
         ],
       ),
