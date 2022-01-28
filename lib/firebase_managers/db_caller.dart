12213b66087109a4e869bb63180f6c4059d2d479
msakac <msakac@foi.hr>
2022-01-28 20:01:02 +0100

    Added methods addCategory, deleteCategory and updateCategory to DBCaller. This methods adds, deletes or edits category to database.

--- lib/firebase_managers/db_caller.dart
+++ lib/firebase_managers/db_caller.dart
@@ -3,15 +3,35 @@ import '../models/user.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DBCaller {
+  static final CollectionReference users =
+      FirebaseFirestore.instance.collection("users");
+  static final CollectionReference categories =
+      FirebaseFirestore.instance.collection("categories");
   //----------User----------//
   static void createUser(User user, String id) {
-    CollectionReference users = FirebaseFirestore.instance.collection("users");
     users.doc(id).set(user.toJson());
   }
-  //----------Categories----------//
-  static void createCategory(CategoryArtwork category){
-    CollectionReference categories = FirebaseFirestore.instance.collection("categories");
-    categories.add(category.toJson()); 
+
+  static Future<void> addCategory(CategoryArtwork category) async {
+    return await categories
+        .add(category.toJson())
+        .then((_) => print("Category added"))
+        .catchError((error) => print("Failed to add category"));
   }
 
+  static Future<void> deleteCategory(String categoryId) async {
+    return await categories
+        .doc(categoryId)
+        .delete()
+        .then((_) => print("Category ${categoryId} deleted"))
+        .catchError((_) => print("Failed to delete category"));
+  }
+
+  static Future<void> updateCategory(CategoryArtwork category) async {
+    return await categories
+        .doc(category.id)
+        .update({'name': category.name})
+        .then((_) => print("Category updated"))
+        .catchError((_) => print("Error while updating"));
+  }
 }
