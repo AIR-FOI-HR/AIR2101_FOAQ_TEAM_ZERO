1f20ecf67597d3b35e657e70917c11227407bc67
msakac <msakac@foi.hr>
2022-01-29 12:10:15 +0100

    Removed that id is required because we get id from firestore when creating museum.

--- lib/models/museum.dart
+++ lib/models/museum.dart
@@ -13,7 +13,7 @@ class Museum with ChangeNotifier {
   //only id and name are required. If sys admin creates new museum he only has to give museum the name.
   //museum admin can then add other info about the museum
   Museum({
-    @required this.id,
+    this.id,
     @required this.name,
     this.description,
     this.address,
