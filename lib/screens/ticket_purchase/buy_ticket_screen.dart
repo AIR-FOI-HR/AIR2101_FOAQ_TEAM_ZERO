39ee8b936cce01c85e336f1f877072a612d381c9
Tom1sl4v97 <tomo.tomiek97@gmail.com>
2022-01-31 20:14:28 +0100

    Changing some design and displaying the tickets data of museum

--- lib/screens/ticket_purchase/buy_ticket_screen.dart
+++ lib/screens/ticket_purchase/buy_ticket_screen.dart
@@ -6,14 +6,17 @@ import '../../widgets/buy_tickets/work_time_item.dart';
 import '../../widgets/app_bar.dart';
 import '../../widgets/buy_tickets/museum_column_data.dart';
 import '../../widgets/my_reservations/elevated_button_my_reservation.dart';
+import '../../widgets/buy_tickets/ticket_data_row.dart';
 
 import '../../models/museum.dart';
 import '../../models/work_time.dart';
 
+import '../../providers/tickets.dart';
 import '../../providers/museums.dart';
 import '../../providers/artworks.dart';
 import '../../providers/categories.dart';
 import '../../providers/work_times.dart';
+import '../../providers/bills.dart';
 
 class BuyTicketScreen extends StatefulWidget {
   static const routeName = "/buy-tickets";
@@ -59,7 +62,7 @@ class _BuyTicketScreenState extends State<BuyTicketScreen> {
     String categoryNames =
         Provider.of<Categories>(context).getCategoryName(artworkProv);
 
-    final museumProv = Provider.of<Museums>(context);
+    final museumProv = Provider.of<Museums>(context, listen: false);
     final Museum museumData = museumProv.getById(museumId);
     int museumTourDuration =
         museumProv.getMuseumTourDuration(museumData.id).toInt();
@@ -70,91 +73,136 @@ class _BuyTicketScreenState extends State<BuyTicketScreen> {
     final workTimeSections = workTimeProv.getWorkTimeSections(
         workTimeData, museumTourDuration, context);
 
+    final ticketsProv = Provider.of<Tickets>(context);
+    final ticketData = ticketsProv.getTickets(museumId);
+
+    final billsProv = Provider.of<Bills>(context);
+    final newBillId = billsProv.createNewBill();
+
     final DateFormat date = DateFormat('dd.MM.yyyy.');
     return Scaffold(
       appBar: appBarProperty,
-      body: Padding(
-        padding: const EdgeInsets.all(5),
-        child: Container(
-          height: (mediaQuery.size.height -
-                  appBarProperty.preferredSize.height -
-                  mediaQuery.padding.top) *
-              0.4,
-          decoration: BoxDecoration(
-            border: Border.all(width: 2),
-          ),
-          width: double.infinity,
-          padding: const EdgeInsets.all(5),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                museumData.name,
-                style: color.textTheme.headline5,
-              ),
-              const SizedBox(height: 10),
-              Row(
-                children: [
-                  Expanded(
-                    flex: 3,
-                    child: Column(
-                      children: [
-                        MuseumColumnData(museumData.address),
-                        const SizedBox(height: 10),
-                        MuseumColumnData('Categories: $categoryNames'),
-                        const SizedBox(height: 10),
-                        MuseumColumnData(workTimeData.timeFrom == null ||
-                                workTimeData.timeTo == null
-                            ? 'Work time:\nClosed'
-                            : 'Work time:\n${workTimeData.timeFrom.format(context)} - ${workTimeData.timeTo.format(context)}'),
-                        const SizedBox(height: 5),
-                        Align(
-                          alignment: Alignment.topLeft,
-                          child: ElevatedButtonMyReservation(
-                              'Select date', () => _selectDate(context)),
-                        ),
-                        const SizedBox(height: 5),
-                        MuseumColumnData(
-                            'Selectad date:\n${date.format(selectedDate.toLocal())}'),
-                      ],
-                    ),
-                  ),
-                  if (workTimeSections.isNotEmpty)
+      body: Column(
+        children: [
+          Container(
+            height: (mediaQuery.size.height -
+                    appBarProperty.preferredSize.height -
+                    mediaQuery.padding.top) *
+                0.4,
+            decoration: BoxDecoration(
+              border: Border.all(width: 2),
+            ),
+            width: double.infinity,
+            padding: const EdgeInsets.all(5),
+            margin: const EdgeInsets.all(5),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  museumData.name,
+                  style: color.textTheme.headline5,
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  children: [
                     Expanded(
-                      flex: 2,
+                      flex: 3,
                       child: Column(
                         children: [
-                          Text(
-                            'Please select time:',
-                            style: color.textTheme.headline4,
+                          MuseumColumnData(museumData.address),
+                          const SizedBox(height: 10),
+                          MuseumColumnData('Categories: $categoryNames'),
+                          const SizedBox(height: 5),
+                          Align(
+                            alignment: Alignment.topLeft,
+                            child: ElevatedButtonMyReservation(
+                                'Select date', () => _selectDate(context)),
                           ),
-                          SizedBox(
-                            height: (mediaQuery.size.height -
-                                    appBarProperty.preferredSize.height -
-                                    mediaQuery.padding.top) *
-                                0.3,
-                            child: GridView.builder(
-                              padding: const EdgeInsets.all(10),
-                              itemCount: workTimeSections.length,
-                              itemBuilder: (ctx, i) =>
-                                  WorkTimeItem(workTimeSections[i]),
-                              gridDelegate:
-                                  const SliverGridDelegateWithFixedCrossAxisCount(
-                                crossAxisCount: 1,
-                                childAspectRatio: 8 / 2,
-                                crossAxisSpacing: 10,
-                                mainAxisSpacing: 10,
-                              ),
-                            ),
-                          ),
+                          const SizedBox(height: 5),
+                          MuseumColumnData(
+                              'Selected date:\n${date.format(selectedDate.toLocal())}'),
+                          const SizedBox(height: 10),
+                          MuseumColumnData(workTimeData.timeFrom == null ||
+                                  workTimeData.timeTo == null
+                              ? 'Work time:\nClosed'
+                              : 'Work time:\n${workTimeData.timeFrom.format(context)} - ${workTimeData.timeTo.format(context)}'),
                         ],
                       ),
-                    )
-                ],
-              ),
-            ],
+                    ),
+                    if (workTimeSections.isNotEmpty)
+                      Expanded(
+                        flex: 2,
+                        child: Column(
+                          children: [
+                            Text(
+                              'Please select time:',
+                              style: color.textTheme.headline4,
+                            ),
+                            SizedBox(
+                              height: (mediaQuery.size.height -
+                                      appBarProperty.preferredSize.height -
+                                      mediaQuery.padding.top) *
+                                  0.3,
+                              child: GridView.builder(
+                                padding: const EdgeInsets.all(10),
+                                itemCount: workTimeSections.length,
+                                itemBuilder: (ctx, i) =>
+                                    WorkTimeItem(workTimeSections[i]),
+                                gridDelegate:
+                                    const SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: 1,
+                                  childAspectRatio: 8 / 2,
+                                  crossAxisSpacing: 10,
+                                  mainAxisSpacing: 10,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                  ],
+                ),
+              ],
+            ),
           ),
-        ),
+          Container(
+            height: (mediaQuery.size.height -
+                    appBarProperty.preferredSize.height -
+                    mediaQuery.padding.top) *
+                0.56,
+            decoration: BoxDecoration(
+              border: Border.all(width: 2),
+            ),
+            width: double.infinity,
+            padding: const EdgeInsets.all(5),
+            margin: const EdgeInsets.all(5),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Tickets',
+                  style: color.textTheme.headline5,
+                ),
+                const SizedBox(height: 15),
+                Container(
+                  padding: const EdgeInsets.all(5),
+                  height: 200,
+                  child: ListView.builder(
+                    itemCount: ticketData.length,
+                    itemBuilder: (_, i) {
+                      return Column(
+                        children: [
+                          TicketDataRow(ticketData[i], i, newBillId),
+                          const Divider(thickness: 1),
+                        ],
+                      );
+                    },
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
