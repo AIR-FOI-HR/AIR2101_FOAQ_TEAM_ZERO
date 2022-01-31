47a98faeafd2ccfe8a496a1c40878f6c3a2466c5
Tom1sl4v97 <tomo.tomiek97@gmail.com>
2022-01-31 13:46:49 +0100

    Creating the button for selecting the museum time to visit

--- lib/widgets/buy_tickets/work_time_item.dart
+++ lib/widgets/buy_tickets/work_time_item.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../providers/bills.dart';
 
 class WorkTimeItem extends StatefulWidget {
   var workTime;
@@ -14,9 +17,33 @@ class _WorkTimeItemState extends State<WorkTimeItem> {
   Widget build(BuildContext context) {
     String openingSection = widget.workTime['openingTime'].format(context);
     String closingSection = widget.workTime['closingTime'].format(context);
+
+    final color = Theme.of(context);
+    final billProv = Provider.of<Bills>(context);
+    final billSelectedTime = billProv.getSelectedTime();
     return FittedBox(
       fit: BoxFit.contain,
-      child: Text('$openingSection - $closingSection'),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30),
+          color: (billSelectedTime == null) ||
+                  (billSelectedTime != widget.workTime['openingTime'])
+              ? Theme.of(context).primaryColorLight
+              : Theme.of(context).highlightColor,
+          boxShadow: kElevationToShadow[6],
+        ),
+        child: FlatButton(
+          child: Text(
+            '$openingSection - $closingSection',
+            style: color.textTheme.headline4,
+          ),
+          onPressed: () {
+            setState(() {
+              billProv.setSelectedTime(widget.workTime['openingTime']);
+            });
+          },
+        ),
+      ),
     );
   }
 }
