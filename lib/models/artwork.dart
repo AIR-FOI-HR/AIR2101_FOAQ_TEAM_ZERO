61b8b257838b78bca8ddcc800c2d328bc54de31c
msakac <msakac@foi.hr>
2022-02-01 17:05:41 +0100

    Added methods toJson and fromSnap for data formating between object and firebase

--- lib/models/artwork.dart
+++ lib/models/artwork.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/foundation.dart';
-import './museum.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class Artwork with ChangeNotifier{
+class Artwork with ChangeNotifier {
   final String id;
   final String name;
   final String imageUrl;
@@ -11,20 +11,40 @@ class Artwork with ChangeNotifier{
   final String category;
   bool isFavorite;
 
-  Artwork({
-  @required this.id,
-  @required this.name,
-  this.imageUrl,
-  this.description,
-  this.author,
-  @required this.museum,
-  @required this.category,
-  this.isFavorite = false
-  });
- 
-void toggleFavorite(){
-  isFavorite = !isFavorite;
-  notifyListeners();
-}
+  Artwork(
+      {this.id,
+      @required this.name,
+      this.imageUrl,
+      this.description,
+      this.author,
+      @required this.museum,
+      @required this.category,
+      this.isFavorite = false});
 
+  static Artwork fromSnap(DocumentSnapshot snap) {
+    var snapshot = snap.data() as Map<String, String>;
+    return Artwork(
+      id: snap.id,
+      category: snap["category"],
+      museum: snap["museum"],
+      name: snap["name"],
+      author: snap["author"],
+      description: snap["description"],
+      imageUrl: snap["imageUrl"],
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+        "category": category,
+        "museum": museum,
+        "name": name,
+        "author": author,
+        "description": description,
+        "imageUrl": imageUrl
+      };
+
+  void toggleFavorite() {
+    isFavorite = !isFavorite;
+    notifyListeners();
+  }
 }
