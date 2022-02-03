10245091f18b2079a24359cef61921e30ee8f879
msakac <msakac@foi.hr>
2022-02-03 08:22:36 +0100

    Removed signOut from appbar. Now when user is logged in we display his avatar and if he clicks the avatar it redirects him to my profile where he can log out.

--- lib/widgets/app_bar.dart
+++ lib/widgets/app_bar.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:museum_app/models/user.dart';
+import 'package:museum_app/screens/my_profile/my_profile_screen.dart';
 import '../screens/login/login_screen.dart';
-import '../firebase_managers/auth_methods.dart';
-
-void logOut() async {
-  await AuthMethods().signOut();
-}
 
 AppBar appBar(String title, BuildContext ctx, Color color, User user) {
-  print("Korisnik id: " + user.toString());
   return AppBar(
     title: Text(title),
     backgroundColor: color,
@@ -27,13 +22,16 @@ AppBar appBar(String title, BuildContext ctx, Color color, User user) {
                   )
                 : IconButton(
                     color: Colors.white,
-                    icon: const Icon(Icons.logout_sharp),
-                    iconSize: 40,
+                    icon: CircleAvatar(
+                      backgroundImage: user.userImage == ""
+                          ? NetworkImage(
+                              "https://thumbs.dreamstime.com/b/default-avatar-profile-icon-vector-social-media-user-image-182145777.jpg")
+                          : NetworkImage(user.userImage),
+                    ),
+                    iconSize: 45,
                     tooltip: 'Log out',
                     onPressed: () {
-                      logOut();
-                      Navigator.of(ctx)
-                          .pushReplacementNamed(LoginScreen.routeName);
+                      Navigator.of(ctx).pushNamed(MyProfileScreen.routeName);
                     },
                   )
           ]
