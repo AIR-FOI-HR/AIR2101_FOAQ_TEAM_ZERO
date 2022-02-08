fde342f1c9b0c5d2a75d9787fac06c8914ac9def
Tom1sl4v97 <tomo.tomiek97@gmail.com>
2022-02-08 17:43:58 +0100

    Creating the provider for the model museum halls

---
+++ lib/providers/museums_halls.dart
@@ -0,0 +1,28 @@
+// ignore_for_file: prefer_final_fields
+
+import 'package:flutter/foundation.dart';
+import '../models/museum_halls.dart';
+
+class MuseumsHalls with ChangeNotifier {
+  List<MuseumHalls> _museumsHalls = [
+    MuseumHalls(
+      id: '1',
+      name: 'Print artwork',
+      order: 1,
+      museumId: '2',
+      categoryId: 'c10',
+    ),
+    MuseumHalls(
+      id: '1',
+      name: 'Painting artwork',
+      order: 1,
+      museumId: '2',
+      categoryId: 'c11',
+    ),
+  ];
+
+  List<MuseumHalls> getMuseumHallsById(String museumId) {
+    return _museumsHalls
+        .where((museumHallsData) => museumHallsData.museumId == museumId);
+  }
+}
