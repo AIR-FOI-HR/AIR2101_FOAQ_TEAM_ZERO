b8b895d8a8c06736810851ff1bb1b9bcd3878332
Tom1sl4v97 <tomo.tomiek97@gmail.com>
2022-02-08 16:13:25 +0100

    Creating the dynamic height and adding the museum button

--- lib/screens/navigation_support/navigation_support_screen.dart
+++ lib/screens/navigation_support/navigation_support_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 import '../../widgets/app_bar.dart';
+import '../../widgets/main_menu_drawer.dart';
+
+import '../../widgets/navigation_support/nav_supp_museum_button.dart';
 
 class NavigationSupportScreen extends StatelessWidget {
   static const routeName = '/navigationSupport';
@@ -9,10 +12,36 @@ class NavigationSupportScreen extends StatelessWidget {
     final color = Theme.of(context);
     final appBarProperty =
         appBar('Naviagtion support', context, color.primaryColor);
+    final mediaQuery = MediaQuery.of(context);
 
     return Scaffold(
       appBar: appBarProperty,
-      body: Text('Too doo'),
+      drawer: MainMenuDrawer(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              height: (mediaQuery.size.height -
+                      appBarProperty.preferredSize.height -
+                      mediaQuery.padding.top) *
+                  0.4,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Choose:',
+                    style: color.textTheme.headline5,
+                  ),
+                  NavSuppMuseumButton('1'),
+                  NavSuppMuseumButton('1'),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
