2ca77c022dcf0160cecbde779f1c33ca50c81e49
Tom1sl4v97 <tomo.tomiek97@gmail.com>
2022-02-09 19:16:35 +0100

    Adding the navigator for the button

--- lib/screens/navigation_support/museum_nav_supp_screen.dart
+++ lib/screens/navigation_support/museum_nav_supp_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import './navigation_support_screen.dart';
+import './museum_nav_supp_crud_screen.dart';
 import '../../widgets/app_bar.dart';
+import '../../widgets/main_menu_drawer.dart';
 import '../../widgets/navigation_support/nav_supp_point_item.dart';
 import '../../widgets/my_reservations/elevated_button_my_reservation.dart';
 
@@ -23,12 +26,12 @@ class MuseumNavSuppScreen extends StatelessWidget {
     final appBarProperty =
         appBar('Navigation support', context, color.primaryColor);
 
-    final Museum museumData = Provider.of<Museums>(context).getById(museumId);
+    final Museum museumData =
+        Provider.of<Museums>(context, listen: false).getById(museumId);
     final List<MuseumHalls> museumHallsData =
-        Provider.of<MuseumsHalls>(context, listen: false)
-            .getMuseumHallsById(museumId);
-    final loggedUserData =
-        Provider.of<Users>(context).findByUsername(loggedUsername);
+        Provider.of<MuseumsHalls>(context).getMuseumHallsById(museumId);
+    final loggedUserData = Provider.of<Users>(context, listen: false)
+        .findByUsername(loggedUsername);
     final bool admin =
         (loggedUserData.userRole == '1' || loggedUserData.userRole == '2') &&
                 loggedUserData.museumId != null
@@ -36,65 +39,73 @@ class MuseumNavSuppScreen extends StatelessWidget {
             : false;
     return Scaffold(
       appBar: appBarProperty,
+      drawer: MainMenuDrawer(),
       body: Padding(
         padding: const EdgeInsets.all(10),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  museumData.name,
-                  style: color.textTheme.headline5,
-                ),
-                SizedBox(
-                  height: 30,
-                  child: ElevatedButtonMyReservation(
-                    'Back',
-                    () {
-                      Navigator.of(context).pop();
-                    },
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    museumData.name,
+                    style: color.textTheme.headline5,
                   ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 10),
-            SizedBox(
-              height: (mediaQuery.size.height -
-                      appBarProperty.preferredSize.height -
-                      mediaQuery.padding.top) *
-                  (admin ? 0.80 : 0.88),
-              child: ListView.builder(
-                itemCount: museumHallsData.length,
-                itemBuilder: (_, i) {
-                  return Column(
-                    children: [
-                      NavSuppPointItem(museumHallsData[i]),
-                      const SizedBox(height: 10),
-                    ],
-                  );
-                },
-              ),
-            ),
-            if (admin)
-              Padding(
-                padding: const EdgeInsets.only(top: 15),
-                child: CircleAvatar(
-                  radius: 25,
-                  backgroundColor: color.buttonColor,
-                  child: IconButton(
-                    color: color.shadowColor,
-                    highlightColor: color.buttonColor,
-                    splashRadius: 30,
-                    iconSize: 35,
-                    icon: const Icon(
-                      Icons.add,
+                  SizedBox(
+                    height: 30,
+                    child: ElevatedButtonMyReservation(
+                      'Back',
+                      () {
+                        Navigator.of(context).pushReplacementNamed(
+                            NavigationSupportScreen.routeName);
+                      },
                     ),
-                    onPressed: () {},
                   ),
+                ],
+              ),
+              const SizedBox(height: 10),
+              SizedBox(
+                height: (mediaQuery.size.height -
+                        appBarProperty.preferredSize.height -
+                        mediaQuery.padding.top) *
+                    (admin ? 0.80 : 0.88),
+                child: ListView.builder(
+                  itemCount: museumHallsData.length,
+                  itemBuilder: (_, i) {
+                    return Column(
+                      children: [
+                        NavSuppPointItem(museumHallsData[i]),
+                        const SizedBox(height: 10),
+                      ],
+                    );
+                  },
                 ),
               ),
-          ],
+              if (admin)
+                Padding(
+                  padding: const EdgeInsets.only(top: 15),
+                  child: CircleAvatar(
+                    radius: 25,
+                    backgroundColor: color.buttonColor,
+                    child: IconButton(
+                      color: color.shadowColor,
+                      highlightColor: color.buttonColor,
+                      splashRadius: 30,
+                      iconSize: 35,
+                      icon: const Icon(
+                        Icons.add,
+                      ),
+                      onPressed: () {
+                        Navigator.of(context).pushNamed(
+                            MuseumNavSuppCrudScreen.routeName,
+                            arguments: {'museumId': museumId});
+                      },
+                    ),
+                  ),
+                ),
+            ],
+          ),
         ),
       ),
     );
