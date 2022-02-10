510d0e630f326b481d92e1849b70ce3bcab3805f
msakac <msakac@foi.hr>
2022-02-10 18:02:34 +0100

    In My profile added row Type. There we will display the user role and museum if user is moderator or owner

--- lib/widgets/my_profile/user_data_column.dart
+++ lib/widgets/my_profile/user_data_column.dart
@@ -5,11 +5,31 @@ import 'package:provider/provider.dart';
 
 import './my_profile_atributs.dart';
 import '../../providers/users.dart';
+import '../../providers/museums.dart';
 
 class UserDataColumn extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final userData = Provider.of<Users>(context).getUser();
+
+    String getType() {
+      int role = int.parse(userData.userRole);
+      if (userData.museumId != "") {
+        final museum = Provider.of<Museums>(context).getById(userData.museumId);
+        if (role == 2) {
+          return "Moderator in:\n ${museum.name}";
+        } else if (role == 3) {
+          return "Museum Owner of:\n ${museum.name}";
+        }
+      } else {
+        if (role == 1) {
+          return "Registered user";
+        } else {
+          return "System Admin";
+        }
+      }
+    }
+
     final divider = Divider(
       thickness: 2,
       color: Theme.of(context).primaryColorDark,
@@ -18,9 +38,9 @@ class UserDataColumn extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        MyProfileAtributs('Name:', userData.name),
+        MyProfileAtributs('Name:', userData.name + " " + userData.surname),
         divider,
-        MyProfileAtributs('Surname:', userData.surname),
+        MyProfileAtributs('Type:', getType()),
         divider,
         MyProfileAtributs('Email:', userData.email),
         divider,
