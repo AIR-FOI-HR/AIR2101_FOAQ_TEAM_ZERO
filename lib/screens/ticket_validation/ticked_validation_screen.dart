678e6b0ba9af2811137917bbd453a0faff00b6ea
msakac <msakac@foi.hr>
2022-02-16 10:01:11 +0100

    Created basic TicketValidationScreen with routeName

---
+++ lib/screens/ticket_validation/ticked_validation_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+
+class TicketValidationScreen extends StatefulWidget {
+  static const routeName = '/ticketValidation';
+  @override
+  _TicketValidationScreenState createState() => _TicketValidationScreenState();
+}
+
+class _TicketValidationScreenState extends State<TicketValidationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
