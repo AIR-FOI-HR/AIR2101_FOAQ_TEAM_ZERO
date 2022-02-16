887cb48fad34266d7a7c024f01291a676d8e98bd
Tom1sl4v97 <tomo.tomiek97@gmail.com>
2022-02-16 16:18:24 +0100

    Creating the route name for map screen

--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,7 @@ import './screens/navigation_support/navigation_support_screen.dart';
 import './screens/navigation_support/museum_nav_supp_screen.dart';
 import './screens/navigation_support/museum_nav_supp_crud_screen.dart';
 import './screens/artworks/favorite_artworks.dart';
+import './screens/map_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -150,6 +151,7 @@ class MyApp extends StatelessWidget {
           FavoriteArtworks.routeName: (ctx) => FavoriteArtworks(),
           AboutUs.routeName: (ctx) => AboutUs(),
           PasswordReset.routeName: (ctx) => PasswordReset(),
+          MapScreen.routeName: (ctx) => MapScreen(),
         },
       ),
     );
